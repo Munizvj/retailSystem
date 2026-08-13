8106ef5588f41290d102d599d69af8cc7dabfd63
Munizvj <joaomuniz319@gmail.com>
2026-08-13 19:47:49 -0300

    implement JWT authentication filter

---
+++ security-service/src/main/java/com/security_service/security/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.security_service.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recoverToken(request);
+        if (tokenJWT != null){
+            String username = securityService.validateToken(tokenJWT);
+
+            if (username != null){
+                var authentication = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+
+    }
+}
